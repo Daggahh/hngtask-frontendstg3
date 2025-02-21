1abd628539ffd39349d958ae9c8f9a2ef6de870c
Dagger <odidihope@gmail.com>
2025-02-22 00:15:56 +0100

    Updated Footer.tsx alignment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const footer = () => {
   return (
-    <footer className="row-start-3 flex gap-6 mt-2 flex-wrap items-center justify-center">
+    <footer className="text-centet row-start-3 flex gap-6 mt-2 flex-wrap items-center justify-center">
       <p className="text-sm md:text-base lg:text-lg font-bold">
         &copy; 2025 AIFlow All-In-One AI powered processor. All Rights Reserved.
       </p>
